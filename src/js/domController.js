b9933b9e2597c5daf996c5794be2b138f0a044e4
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-06-12 09:46:55 +0100

    Add funcs to show/hide search overlay
    Search overlay will be displayed if no weather data exists in storage

--- src/js/domController.js
+++ src/js/domController.js
@@ -132,6 +132,31 @@ export function addHourlyForecastToDOM() {
   });
 }
 
+export function showSearchOverlay() {
+  const container = document.querySelector("main");
+
+  container.insertAdjacentHTML(
+    "beforebegin",
+    `
+    <div class="search-overlay-container">
+      <div class="logo-container">
+        <h1 class="logo">weather<span>forecast</span></h1>
+      </div>
+      <form id="search-overlay-form">
+        <input type="search" id="search-overlay-input" name="search-overlay-input" placeholder="Search location..." />
+        <button id="overlay-clear-button" type="reset" title="Clear"><i class="fa-solid fa-xmark"></i></button>
+        <button id="overlay-search-button" type="submit" title="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+    </div>
+    `
+  );
+}
+
+export function hideSearchOverlay() {
+  const overlay = document.querySelector(".search-overlay-container");
+  overlay.classList.add("fade-out");
+}
+
 export function setSearchInputValue(val) {
   const search = document.getElementById("search");
   search.value = val;
